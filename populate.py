db149b73be9409aa72647a0b468d499109d9d624
Brian C Sparks <briancsparks@gmail.com>
2023-05-11 04:58:14 +0000

    wip

--- populate.py
+++ populate.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+
 import influxdb_client, os, time
 from influxdb_client import InfluxDBClient, Point, WritePrecision
 from influxdb_client.client.write_api import SYNCHRONOUS
